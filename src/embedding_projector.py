937ae94457987cf8729c9a90e24dfcc863d1003a
Alessandro De Luca (aledel) <aledel@LINGUISTIKMN-18.d.uzh.ch>
2023-09-11 11:30:30 +0200

    TensorBoardTool class

--- src/embedding_projector.py
+++ src/embedding_projector.py
@@ -5,76 +5,82 @@ Alessandro De Luca - 09/2023
 """
 import os
 import subprocess
+import logging
 import numpy as np
 import tensorflow as tf
 from tensorboard import program
 from tensorboard.plugins import projector
 
 
-def _data_setup(
-        embedding_vecs: np.ndarray, metadata: np.ndarray,
-        metadata_var: list, log_dir='for_tensorboard/logs/') -> None:
-    """Prepares data for embedding projector.
+class TensorBoardTool:
 
-    Args:
-        embedding_vecs: Array of embedding vectors.
-        metadata: Array of metadata variables.
-        metadata_vars: Array of metadata variable names.
-        log_dir: Path to log directory for TensorBoard.
-    """
+    def __init__(self, log_dir, embedding_vecs, metadata, metadata_var):
+        """Initialize TensorBoardTool class.
 
-    # Make log directory
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+        Args:
+            log_dir: Path to log directory for TensorBoard.
+            embedding_vecs: Array of embedding vectors.
+            metadata: Array of metadata variables.
+            metadata_vars: Array of metadata variable names.
+        """
+        self.log_dir = log_dir
+        self.embedding_vecs = embedding_vecs
 
-    # Embedding variable initialized with embeddings
-    embedding_var = tf.Variable(embedding_vecs, name='embedding')
+        # Make metadata variable name a list if not already
+        if type(metadata_var) is not list:
+            self.metadata_var = [metadata_var]
+        else:
+            self.metadata_var = metadata_var
 
-    # Create a checkpoint from embedding, the filename and key are the
-    # name of the tensor.
-    checkpoint = tf.train.Checkpoint(embedding=embedding_var)
-    checkpoint.save(os.path.join(log_dir, "embedding.ckpt"))
+        # Reshape metadata if 1D
+        if len(self.metadata_var) == 1 and len(metadata.shape) == 1:
+            self.metadata = metadata.reshape((metadata.shape[0], 1))
+        else:
+            self.metadata = metadata
 
-    # Make metadata variable name a list if not already
-    if type(metadata_var) is not list:
-        metadata_var = [metadata_var]
+    def _data_setup(self) -> None:
+        """Prepares data for embedding projector.
+        """
+        # Make log directory
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
 
-    # Reshape metadata if 1D
-    if len(metadata_var) == 1 and len(metadata.shape) == 1:
-        metadata = metadata.reshape((metadata.shape[0], 1))
+        # Embedding variable initialized with embeddings
+        self.embedding_var = tf.Variable(self.embedding_vecs, name='embedding')
 
-    # Metadata file
-    with open(os.path.join(log_dir, 'metadata.tsv'), 'w') as f:
-        if len(metadata_var) > 1:
-            # Write labels for metadata if there are more than 1 vars
-            f.write('\t'.join(metadata_var)+"\n")
+        # Create a checkpoint from embedding, the filename and key are the
+        # name of the tensor.
+        self.checkpoint = tf.train.Checkpoint(embedding=self.embedding_var)
+        self.checkpoint.save(os.path.join(self.log_dir, "embedding.ckpt"))
 
-        # Write metadata labels
-        for labels in metadata:
-            f.write('\t'.join(labels)+"\n")
+        # Metadata file
+        with open(os.path.join(self.log_dir, 'metadata.tsv'), 'w') as f:
+            if len(self.metadata_var) > 1:
+                # Write labels for metadata if there are more than 1 vars
+                f.write('\t'.join(self.metadata_var)+"\n")
 
+            # Write metadata labels
+            for labels in self.metadata:
+                f.write('\t'.join(labels)+"\n")
 
-def projector_setup(
-        embedding_vecs: np.ndarray, metadata: np.ndarray,
-        metadata_var: list, log_dir='for_tensorboard/logs/') -> None:
-    """Sets up embedding projector and projects the embeddings.
+    def _projector_setup(self) -> None:
+        """Sets up embedding projector and projects the embeddings.
+        """
+        # Prepare data for embedding projector
+        self._data_setup()
 
-    Args
-        embedding_vecs: Array of embedding vectors.
-        metadata: Array of metadata variables.
-        metadata_vars: Array of metadata variable names.
-        log_dir: Path to the log directory for TensorBoard.
-    """
-    # Prepare data for embedding projector
-    _data_setup(
-        embedding_vecs, metadata, metadata_var, log_dir)
+        # Configure embedding projector
+        self.config = projector.ProjectorConfig()
+        self.embedding = self.config.embeddings.add()
+        self.embedding.tensor_name = 'embedding/.ATTRIBUTES/VARIABLE_VALUE'
+        self.embedding.metadata_path = 'metadata.tsv'
+        projector.visualize_embeddings(self.log_dir, self.config)
 
-    # Configure embedding projector
-    config = projector.ProjectorConfig()
-    embedding = config.embeddings.add()
-    embedding.tensor_name = 'embedding/.ATTRIBUTES/VARIABLE_VALUE'
-    embedding.metadata_path = 'metadata.tsv'
-    projector.visualize_embeddings(log_dir, config)
+    def run(self):
+        self._projector_setup()
+        tb = program.TensorBoard()
+        tb.configure(argv=[None, '--logdir', self.log_dir])
+        tb.main()
 
 
 def test():
@@ -91,16 +97,11 @@ def test():
     metadata_vars = ['speaker']
 
     # Run tensorflow embedding projector setup
-    projector_setup(embedding_vecs=embeddings, metadata=metadata,
-                    metadata_var=metadata_vars, log_dir=log_dir)
-
-    # Launch TensorBoard
-    # subprocess.run('tensorboard --logdir for_tensorboard/logs/test')
-    tb = program.TensorBoard()
-    tb.configure(argv=[None, '--logdir', log_dir])
-    # url = tb.launch()
-    # print(f"TensorBoard listening on {url}")
-    tb.main()
+    tbTool = TensorBoardTool(
+        log_dir=log_dir, embedding_vecs=embeddings,
+        metadata=metadata, metadata_var=metadata_vars
+    )
+    tbTool.run()
 
 
 if __name__ == '__main__':
