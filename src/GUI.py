ea6e1f1cd992eb8b11a3370d472ab11d6a5b550a
Alessandro De Luca <alessandro.deluca@ieu.uzh.ch>
2023-11-09 22:59:26 +0100

    init gui script

---
+++ src/GUI.py
@@ -0,0 +1,26 @@
+"""
+Graphical User Interface for SpEmViz built using tkinter.
+"""
+
+import tkinter as tk
+
+class gui():
+    """
+    Tkinter GUI for SpEmViz.
+
+    Parameters:
+    -----------
+    """
+    def __init__(self):
+        self.root = tk.Tk()     # root window
+
+        self.label = tk.Label(self.root, text="Test message", font=('Arial', 18))
+        self.label.pack(padx=10, pady=10)
+
+        self.savepath = tk.Text(self.root, height=1, font=('Arial', 16))
+        self.savepath.pack(padx=10, pady=10)
+
+        self.root.mainloop()    # run mainloop
+
+if __name__ == '__main__':
+    gui()
