6f3bafb77ef1687f97ce371b8bb48e00757be5cc
delale@MacBookPro <a.deluca.italia@gmail.com>
2023-12-05 18:09:28 +0100

    SpeakerEmbedder class

--- lib/speaker_embeddings.py
+++ lib/speaker_embeddings.py
@@ -1,8 +1,10 @@
 # Speaker embeddings using ECAPA-TDNN VoxCeleb
 # TODO: add speaker verification option
 
+from collections import defaultdict
 import warnings
 import os
+from typing import Tuple
 import numpy as np
 from speechbrain.pretrained import EncoderClassifier
 import torch
@@ -10,49 +12,7 @@ import torch.nn.functional as F
 import torchaudio
 
 
-def _load_audio_files(wav_files: list) -> torch.Tensor:
-    """
-    Audio files loader. Also makes sure that sampling rate is 16000 Hz.
-
-    Parameters:
-    -----------
-    wav_files: list
-        List of audio file paths.
-
-    Returns:
-    --------
-    wavs : torch.Tensor
-        Tensor of audio files.
-    wav_lens: torch.Tensor
-        Tensor of audio file lengths relative to longest audio file.
-    """
-    # load files
-    wavs: list = [None] * len(wav_files)
-    max_len = 0
-    for i, wav_file in enumerate(wav_files):
-        signal, sr = torchaudio.load(wav_file)
-        if sr != 16000:  # make sure sampling rate = 16000 Hz
-            signal = torchaudio.functional.resample(
-                signal, orig_freq=sr, new_freq=16000
-            )
-        if signal.shape[1] > max_len:
-            max_len = signal.shape[1]
-
-        wavs[i] = signal
-
-    # pad files
-    wav_lens = [sig.shape[1] / max_len for sig in wavs]
-    wavs = [F.pad(sig, (0, max_len - sig.shape[1]),
-                  mode='constant', value=0) for sig in wavs]
-
-    # cat tensors
-    wavs = torch.cat(wavs, dim=0)
-    wav_lens = torch.tensor(wav_lens)
-
-    return wavs, wav_lens
-
-
-def spembed(audio_dir: str) -> np.ndarray:
+class SpeakerEmbedder:
     """
     Speaker embedding using ECAPA-TDNN VoxCeleb.
 
@@ -60,44 +20,204 @@ def spembed(audio_dir: str) -> np.ndarray:
     -----------
     audio_dir: str
         Directory containing audio files. Can also be a single audio file.
+    metadata_vars : dict
+        Dictionary of metadata variables to extract from the filename. Keys are 
+        metavars: list of names of metadata variables to extract from the filename
+        (use '-' to ignore the variable). Should have the same index as variable in 
+        filename after splitting by separator;
+        separator: the separating character between metadata variables in the filename.
+        Example:
+        metadata_vars = {
+            'metavars': ['speaker', 'emotion', '-', 'selection'],
+            'separator': '_'
+        }
 
-    Returns:
-    --------
-    np.ndarray
-        Speaker embedding (N files, 192).
+    Attributes:
+    -----------
+    audio_dir: str
+        Directory containing audio files. Can also be a single audio file.
+    metadata_vars : dict
+        Dictionary of metadata variables to extract from the filename. Keys are 
+        metavars: list of names of metadata variables to extract from the filename
+        (use '-' to ignore the variable). Should have the same index as variable in 
+        filename after splitting by separator;
+        separator: the separating character between metadata variables in the filename.
+        Example:
+        metadata_vars = {
+            'metavars': ['speaker', 'emotion', '-', 'selection'],
+            'separator': '_'
+        }
     """
 
-    if os.path.isdir(audio_dir):
-        audio_files: list = [
-            os.path.join(audio_dir, f) for f in os.listdir(audio_dir) if f.endswith('.wav')
-        ]
-        if len(audio_files) == 0:
-            raise FileNotFoundError('No .wav files found in audio_dir')
-    elif audio_dir.endswith('.wav'):
-        audio_files: list = [audio_dir]
-    else:
-        raise FileNotFoundError(
-            'audio_dir must be a directory containing .wav files or a .wav file')
+    def __init__(self, audio_dir: str, metadata_vars: dict = {'metavars': None, 'separator': None}):
+        self.audio_dir: str = audio_dir
+        self.metadata_vars: dict = metadata_vars
+        if os.path.isdir(self.audio_dir):
+            self.audio_files: list = [os.path.join(self.audio_dir, f
+                                                   ) for f in os.listdir(self.audio_dir) if f.endswith('.wav')
+                                      ]
+            if len(self.audio_files) == 0:
+                raise FileNotFoundError('No .wav files found in audio_dir')
+        elif self.audio_dir.endswith('.wav'):
+            self.audio_files: list = [self.audio_dir]
+        else:
+            raise FileNotFoundError(
+                'audio_dir must be a directory containing .wav files or a .wav file')
 
-    # load files
-    wavs: torch.Tensor
-    wav_lens: torch.Tensor
-    wavs, wav_lens = _load_audio_files(audio_files)
+        # Init pretrained model
+        if not os.path.isdir('.pretrained_classifier'):
+            os.mkdir('.pretrained_classifier')
+        self.classifier: EncoderClassifier = EncoderClassifier.from_hparams(
+            source='speechbrain/spkrec-ecapa-voxceleb',
+            savedir='.pretrained_classifier'
+        )
 
-    # init classifier
-    if not os.path.isdir('.pretrained_classifier'):
-        os.mkdir('.pretrained_classifier')
-    classifier: EncoderClassifier = EncoderClassifier.from_hparams(
-        source='speechbrain/spkrec-ecapa-voxceleb',
-        savedir='.pretrained_classifier'
-    )
+    # Helper to load audio files as tensors
 
-    # embed
-    embedding: torch.Tensor = classifier.encode_batch(
-        wavs=wavs, wav_lens=wav_lens)
+    def _load_audio_files(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Audio files loader. Loads into a torch.Tensor the wav files making sure 
+        that the sampling rate is 16000 Hz. The files are padded with zeros to
+        match the length of the longest file. The tensor is then reshaped to
+        (N files, max_len) where max_len is the length of the longest file.
+        It also creates a torch.Tensor containing the length of each file relative
+        to the longest file.
 
-    # reshape and transform to numpy
-    embedding: torch.Tensor = torch.reshape(embedding, shape=(
-        embedding.shape[0], 192)).numpy()  # 192 embeddings
+        Returns:
+        --------
+        wavs: torch.Tensor
+            Tensor of audio files.
+        wav_lens: torch.Tensor
+            Tensor of relative audio file lengths.
+        """
+        # load files
+        wavs: list = [None] * len(self.audio_files)
+        max_len = 0
+        for i, wav_file in enumerate(self.audio_files):
+            signal, sr = torchaudio.load(wav_file)
+            if sr != 16000:  # make sure sampling rate = 16000 Hz
+                signal = torchaudio.functional.resample(
+                    signal, orig_freq=sr, new_freq=16000
+                )
+            if signal.shape[1] > max_len:
+                max_len = signal.shape[1]
 
-    return embedding
+            wavs[i] = signal
+
+        # pad files
+        wav_lens = [sig.shape[1] / max_len for sig in wavs]
+        wavs = [F.pad(sig, (0, max_len - sig.shape[1]),
+                      mode='constant', value=0) for sig in wavs]
+
+        # cat tensors
+        wavs = torch.cat(wavs, dim=0)  # tensor of audio files
+        # tensor of relative audio file lengths
+        wav_lens = torch.tensor(wav_lens)
+
+        return wavs, wav_lens
+
+    def spembed(self, wavs, wav_lens) -> np.ndarray:
+        """
+        Speaker embedding using ECAPA-TDNN VoxCeleb.
+
+        Parameters:
+        -----------
+        wavs: torch.Tensor
+            Tensor of audio files.
+        wav_lens: torch.Tensor
+            Tensor of relative audio file lengths.
+
+        Returns:
+        --------
+        np.ndarray
+            Speaker embedding (N files, 192).
+        """
+        # embed
+        embedding: torch.Tensor = self.classifier.encode_batch(
+            wavs=wavs, wav_lens=wav_lens)
+
+        # reshape and transform to numpy
+        embedding: np.ndarray = torch.reshape(embedding, shape=(
+            embedding.shape[0], 192)).numpy()  # 192 embeddings
+
+        return embedding
+
+    def extract_metadata(self, metavars: list = None, separator: str = '_') -> dict:
+        """
+        Extracts metadata variables from the filename.
+
+        Parameters:
+        -----------
+        metavars: list
+            List of metadata variable names. Metadata variable should have the same index as
+            variable in filename after splitting by separator. If '-' then variable is skipped.
+            If None, returns only filename.
+        separator: str
+            Separator character between metadata variables.
+
+        Returns:
+        --------
+        dict
+            Dictionary of metadata variables. If metavars is None, returns only filename.
+        """
+        metadata_dict: defaultdict = defaultdict(list)  # init empty dict
+
+        for f in self.audio_files:
+            basename = os.path.basename(f)
+            if metavars:
+                metadata: list = os.path.splitext(basename)[0].split(separator)
+
+            metadata_dict['filename'].append(basename)
+            for i, var in enumerate(metavars):
+                if var != '-':
+                    metadata_dict[var].append(metadata[i])
+
+        return metadata_dict
+
+    def process_files(self):
+        """
+        Process files.
+
+        Returns:
+        --------
+        features: np.ndarray
+            Speaker embeddings. Shape (N files, 192).
+        metadata_values: np.ndarray
+            Extracted metadata. Shape is (N files, n_metavars).
+        metadata_labels: list
+            Metadata variable names.
+        """
+        # load audio files
+        wavs, wav_lens = self._load_audio_files()
+
+        # embed
+        features: np.ndarray = self.spembed(wavs=wavs, wav_lens=wav_lens)
+
+        # extract metadata
+        metadata_dict: dict = self.extract_metadata(**self.metadata_vars)
+
+        # convert to numpy
+        metadata_labels: list = list(metadata_dict.keys())
+        metadata_values: np.ndarray = np.array(list(metadata_dict.values())).T
+
+        return features, metadata_values, metadata_labels
+
+
+# debugging
+if __name__ == '__main__':
+    audio_dir = 'tests/data/'
+    feature_methods = {
+        'mel_features': {'deltas': True, 'summarise': True, 'n_mfcc': 13},
+        'acoustic_features': {'f0min': 75.0, 'f0max': 600.0},
+        'low_lvl_features': {'use_mean_contrasts': True, 'summarise': True},
+        'lpc_features': {'summarise': True}
+    }
+    metadata_vars = {
+        'metavars': ['speaker', '-', 'sentence'],
+        'separator': '_'
+    }
+    se = SpeakerEmbedder(audio_dir, metadata_vars)
+    features, metadata_values, metadata_labels = se.process_files()
+    print(features.shape)
+    print(metadata_values.shape)
+    print(metadata_labels)
--- tests/test_speaker_embeddings.py
+++ tests/test_speaker_embeddings.py
@@ -1,27 +1,51 @@
 import os
 import unittest
 import numpy as np
-from ..lib.speaker_embeddings import spembed
+import torch
+from ..lib.speaker_embeddings import SpeakerEmbedder
 
 
 class TestSpembed(unittest.TestCase):
     def setUp(self) -> None:
-        self.audio_dir = 'tests/data'
-        self.audio_file = 'tests/data/01_test_001.wav'
+        audio_dir = 'tests/data/'
+        metadata_vars = {
+            'metavars': ['speaker', '-', 'sentence'],
+            'separator': '_'
+        }
+        self.se = SpeakerEmbedder(audio_dir, metadata_vars)
 
-    def test_spembed_dir(self):
-        result = spembed(self.audio_dir)
-        self.assertIsInstance(result, np.ndarray)
-        self.assertEqual(result.shape, (5, 192))
+    def test_audio_files_exist(self):
+        self.assertTrue(all(os.path.exists(f)
+                        for f in self.se.audio_files))
+
+    def test_load_dir(self):
+        result_wavs, result_wav_lens = self.se._load_audio_files()
+        self.assertIsInstance(result_wavs, torch.Tensor)
+        self.assertEqual(result_wavs.shape[0], 5)
+        self.assertIsInstance(result_wav_lens, torch.Tensor)
+        self.assertEqual(result_wav_lens.shape[0], 5)
 
     def test_spembed_file(self):
-        result = spembed(self.audio_file)
+        se = SpeakerEmbedder('tests/data/01_test_001.wav')
+        wavs, wav_lens = se._load_audio_files()
+        result = se.spembed(wavs=wavs, wav_lens=wav_lens)
         self.assertIsInstance(result, np.ndarray)
         self.assertEqual(result.shape, (1, 192))
 
-    def test_spembed_file_not_found(self):
-        with self.assertRaises(FileNotFoundError):
-            spembed('invalid_input')
+    def test_process_files(self):
+        result_features, result_metadata_values, result_metadata_labels = self.se.process_files()
+
+        # Type checks
+        self.assertIsInstance(result_features, np.ndarray)
+        self.assertIsInstance(result_metadata_values, np.ndarray)
+        self.assertIsInstance(result_metadata_labels, list)
+
+        # Result has shape (n_files, n_features)
+        self.assertEqual(result_features.shape,
+                         (len(self.se.audio_files), 192))
+        self.assertEqual(result_metadata_values.shape,
+                         (len(self.se.audio_files), 3))
+        self.assertEqual(len(result_metadata_labels), 3)
 
 
 if __name__ == '__main__':
